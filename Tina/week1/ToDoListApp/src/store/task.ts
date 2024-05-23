62342eff8292ad72908c245d3d96474d6ee6a9db
kwaksj329 <kwaksj329@gmail.com>
2024-05-24 00:26:17 +0900

    feat: AsyncStorage에 theme, task 저장 기능 추가

--- Tina/week1/ToDoListApp/src/store/task.ts
+++ Tina/week1/ToDoListApp/src/store/task.ts
@@ -1,4 +1,6 @@
 import {create} from 'zustand';
+import {persist, createJSONStorage} from 'zustand/middleware';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export interface TaskProps {
   task: string;
@@ -12,9 +14,17 @@ export interface TaskStoreProps {
   setTaskList: (input: Array<TaskProps>) => void;
 }
 
-export const useTask = create<TaskStoreProps>(set => ({
-  task: '',
-  taskList: [],
-  setTask: (input: string) => set({task: input}),
-  setTaskList: (input: Array<TaskProps>) => set({taskList: [...input]}),
-}));
+export const useTask = create<TaskStoreProps, [['zustand/persist', unknown]]>(
+  persist(
+    set => ({
+      task: '',
+      taskList: [],
+      setTask: (input: string) => set({task: input}),
+      setTaskList: (input: Array<TaskProps>) => set({taskList: [...input]}),
+    }),
+    {
+      name: 'task-storage',
+      storage: createJSONStorage(() => AsyncStorage),
+    },
+  ),
+);
