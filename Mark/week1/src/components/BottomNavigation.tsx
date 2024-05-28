67e3744dd11ffa61e993b7ac03b3d67664cb8822
Lee-Sunho <sunho9889@naver.com>
2024-05-28 14:36:25 +0900

    design: 바텀 네비게이션 스타일링

--- Mark/week1/src/components/BottomNavigation.tsx
+++ Mark/week1/src/components/BottomNavigation.tsx
@@ -5,31 +5,35 @@ import Setting from '../screens/Setting';
 import HomeSvg from '../assets/icon_home.svg';
 import SettingSvg from '../assets/icon_setting.svg';
 import BtnAddSvg from '../assets/btn_add.svg';
-import { TouchableOpacity } from 'react-native';
+import { StyleSheet, TouchableOpacity } from 'react-native';
+import { useStore } from '../store/store';
+import palette from '../constants/palette';
 
 const Tab = createBottomTabNavigator<RootTabParamList>();
 
 const BottomNavigation = () => {
   const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const color = useStore((state) => state.color);
 
   const handlePress = () => {
     navigation.navigate('AddToDo');
   }
 
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false }}>
+    <Tab.Navigator screenOptions={{ headerShown: false, tabBarItemStyle: styles.tab }}>
       <Tab.Screen name='Home' component={Home} options={{
         title: '홈',
         tabBarLabel: '홈',
+        tabBarActiveTintColor: color,
         tabBarIcon: ({focused}) => (
-          <HomeSvg color={focused ? '#FF8F50': '#888888'}/>
+          <HomeSvg color={focused ? color: palette.darkGray}/>
         ),
       }}/>
 
       <Tab.Screen name='AddToDo' component={Home} options={{
         tabBarLabel: () => null,
-        tabBarIcon: () => (
-          <TouchableOpacity onPress={handlePress}>
+        tabBarButton: () => (
+          <TouchableOpacity style={styles.addBtn} onPress={handlePress}>
             <BtnAddSvg />
           </TouchableOpacity>
         )
@@ -38,13 +42,25 @@ const BottomNavigation = () => {
       <Tab.Screen name='Setting' component={Setting} options={{
         title: '설정',
         tabBarLabel: '설정',
+        tabBarActiveTintColor: color,
         tabBarIcon: ({focused}) => (
-          <SettingSvg color={focused ? '#FF8F50': '#888888'}/>
+          <SettingSvg color={focused ? color: palette.darkGray}/>
         ),
       }}/>
     </Tab.Navigator>
   )
 }
+
+const styles = StyleSheet.create({
+  tab: {
+    paddingVertical: 6,
+  },
+  addBtn: {
+    width: 38,
+    height: 38,
+    alignSelf: 'center'
+  }
+})
 
 export default BottomNavigation;
        
---
+++ Mark/week1/src/store/store.ts
@@ -0,0 +1,15 @@
+import { create } from 'zustand';
+import palette from '../constants/palette';
+
+interface Store {
+  color: string;
+  todos: [];
+  setColor: (color: string) => void;
+  setToDos: (todos: []) => void;
+}
+export const useStore = create<Store>((set)=>({
+  color: palette.orange,
+  todos: [],
+  setColor: (state: string) => set({color: state}),
+  setToDos: (state: []) => set({todos: [...state]}),
+}))
