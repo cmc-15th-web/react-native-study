d9bf2e2b1e2f39a059daf11fe7efc0b2c19c739a
joseph0926 <joseph0926@naver.com>
2024-06-08 08:47:38 +0900

    feat: [11] 위치,줌 업데이트 기능 추가

--- Younghoon/week2/rn/components/webivew-test.tsx
+++ Younghoon/week2/rn/components/webivew-test.tsx
@@ -1,6 +1,6 @@
 import { useLocation } from "@/hooks/use-location";
-import { useRef, useEffect, useState } from "react";
-import { Alert, Platform } from "react-native";
+import { useRef } from "react";
+import { Platform } from "react-native";
 import { WebView, WebViewMessageEvent } from "react-native-webview";
 
 export function WebviewTest() {
@@ -8,35 +8,11 @@ export function WebviewTest() {
 
   const webViewRef = useRef<WebView>(null);
 
-  useEffect(() => {
-    if (webViewRef.current && location) {
-      const { latitude, longitude } = location.coords;
-      webViewRef.current.postMessage(
-        JSON.stringify({ type: "location", latitude, longitude })
-      );
-    }
-  }, [location]);
-
-  const injectCustomJavascript = () => {};
-
   const onMessage = (event: WebViewMessageEvent) => {
     try {
       const message = JSON.parse(event.nativeEvent.data);
 
       console.log(message);
-
-      if (message.type === "getLocation") {
-        if (location) {
-          const { latitude, longitude } = location.coords;
-          webViewRef.current?.postMessage(
-            JSON.stringify({
-              type: "location",
-              latitude,
-              longitude,
-            })
-          );
-        }
-      }
     } catch (error) {
       console.error("parse message error: ", error);
     }
@@ -47,13 +23,12 @@ export function WebviewTest() {
       <WebView
         originWhitelist={["*"]}
         source={{
-          uri: `http://172.30.1.97:5173`,
+          uri: `http://172.30.1.100:5173`,
         }}
         javaScriptEnabled={true}
         userAgent={`webview-${Platform.OS === "ios" ? "ios" : "android"}`}
         ref={webViewRef}
         onMessage={onMessage}
-        onLoadEnd={injectCustomJavascript}
         injectedJavaScriptObject={{ location }}
       />
     </>
--- Younghoon/week2/web/src/components/mapbox-map.tsx
+++ Younghoon/week2/web/src/components/mapbox-map.tsx
@@ -1,59 +1,117 @@
 import { Map } from 'mapbox-gl';
 import { useEffect, useRef, useState } from 'react';
 import { Button } from './ui/button';
+import { Minus, Plus } from 'lucide-react';
 
 export function MapboxMap() {
   const map = useRef<Map | undefined>(undefined);
   const mapContainer = useRef<HTMLDivElement | null>(null);
 
+  const [initialLocationSet, setInitialLocationSet] = useState<boolean>(false);
+  const [currentLng, setCurrentLng] = useState<number>(0);
+  const [currentLat, setCurrentLat] = useState<number>(0);
   const [lng, setLng] = useState<number>(-70.9);
   const [lat, setLat] = useState<number>(42.35);
-  const [zoom, setZoom] = useState<number>(9);
+  const [zoom, setZoom] = useState<number>(15);
 
   useEffect(() => {
     if (map.current) return;
     map.current = new Map({
       container: mapContainer.current as HTMLElement | string,
       style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom: zoom,
+      center: [currentLng, currentLat],
+      zoom,
     });
-  }, [lng, lat]);
+  }, [currentLat, currentLng, zoom]);
 
   useEffect(() => {
     if (window.ReactNativeWebView) {
       window.ReactNativeWebView.postMessage(
-        JSON.stringify({ type: 'test', data: { lng, lat } }),
+        JSON.stringify({ type: 'test', data: { lng, lat, zoom } }),
       );
     }
-  }, [lng, lat]);
+  }, [lng, lat, zoom]);
 
   useEffect(() => {
     if (
       window.ReactNativeWebView &&
-      window.ReactNativeWebView.injectedObjectJson()
+      window.ReactNativeWebView?.injectedObjectJson()
     ) {
       const location = JSON.parse(
-        window.ReactNativeWebView.injectedObjectJson(),
-      ).location;
-      const { latitude, longitude } = location.coords;
-      map.current?.setCenter({ lat: latitude, lng: longitude });
-      setLat(latitude);
-      setLng(longitude);
+        window.ReactNativeWebView?.injectedObjectJson(),
+      )?.location;
+      if (location && !initialLocationSet) {
+        const { latitude, longitude } = location.coords;
+        map.current?.setCenter([longitude, latitude]);
+        setCurrentLat(latitude);
+        setCurrentLng(longitude);
+        setInitialLocationSet(true);
+      }
+    }
+  }, [initialLocationSet]);
+
+  useEffect(() => {
+    map.current?.setZoom(zoom);
+  }, [zoom]);
+
+  useEffect(() => {
+    if (map.current) {
+      map.current.on('move', () => {
+        setLng(map.current!.getCenter().lng);
+        setLat(map.current!.getCenter().lat);
+        setZoom(map.current!.getZoom());
+      });
     }
   }, []);
 
   return (
-    <div>
-      <Button
-        variant="default"
-        size="default"
-        className="absolute left-4 top-4 z-50"
-        onClick={() => location.reload()}
-      >
-        내 위치로 이동
-      </Button>
-      <div ref={mapContainer} className="h-screen w-screen" />
+    <div className="h-screen w-screen overflow-hidden">
+      <div className="absolute left-4 top-4 z-50 flex w-screen items-center justify-between pr-8">
+        <Button
+          variant="ghost"
+          size="default"
+          className="bg-[#5061FF] text-white hover:bg-[#5061FF] hover:text-white"
+          onClick={() => {
+            map.current?.setCenter([currentLng, currentLat]);
+            map.current?.setZoom(15);
+          }}
+        >
+          내 위치로 이동
+        </Button>
+        <div className="flex items-center gap-1">
+          <Button
+            variant="ghost"
+            className="bg-[#5061FF] text-white hover:bg-[#5061FF] hover:text-white"
+            size="sm"
+            disabled={zoom >= 18}
+            onClick={() => {
+              if (zoom >= 18) {
+                setZoom(18);
+                return;
+              }
+              setZoom((prevState) => prevState + 1);
+            }}
+          >
+            <Plus className="h-4 w-4" />
+          </Button>
+          <Button
+            variant="ghost"
+            className="bg-[#5061FF] text-white hover:bg-[#5061FF] hover:text-white"
+            disabled={zoom <= 1}
+            size="sm"
+            onClick={() => {
+              if (zoom <= 1) {
+                setZoom(1);
+                return;
+              }
+              setZoom((prevState) => prevState - 1);
+            }}
+          >
+            <Minus className="h-4 w-4" />
+          </Button>
+        </div>
+      </div>
+      <div ref={mapContainer} className="h-full w-full" />
     </div>
   );
 }
