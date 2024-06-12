e2584e22401f52889e297d36d9d02605cf557e6b
TaegeonKim <aksen5240@gmail.com>
2024-06-12 13:39:26 +0900

    feat: Vite로 웹 프로젝트 생성 및 ReactNative WebView와 연동

--- Taeryong/week2/ReactNative/babel.config.js
+++ Taeryong/week2/ReactNative/babel.config.js
@@ -1,6 +1,17 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
+  presets: ['module:@react-native/babel-preset', '@babel/preset-typescript'],
   plugins: [
+    [
+      'module:react-native-dotenv',
+      {
+        envName: 'APP_ENV',
+        moduleName: '@env',
+        path: '.env',
+        safe: false,
+        allowUndefined: true,
+        verbose: false,
+      },
+    ],
     [
       'module-resolver',
       {
--- Taeryong/week2/ReactNative/src/screens/Home.tsx
+++ Taeryong/week2/ReactNative/src/screens/Home.tsx
@@ -1,18 +1,21 @@
-import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {WebView} from 'react-native-webview';
+import {API_URL} from '@env';
 
-const Home = () => {
+const App = () => {
+  console.log('🚀 ~ file: home.tsx:5 ~ API_URL:', API_URL);
   return (
-    <View style={styles.container}>
-      <Text>home</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <WebView source={{uri: API_URL}} style={styles.container} />
+    </SafeAreaView>
   );
 };
-
-export default Home;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
 });
+
+export default App;
