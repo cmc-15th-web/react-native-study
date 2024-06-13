d0bd88e5b744a87b8c2764928000dc19e47b8542
yeongeun <duddms0190@naver.com>
2024-06-13 14:32:02 +0900

    feat: 현재위치 전송

--- Ino/week2/mapApp/App.tsx
+++ Ino/week2/mapApp/App.tsx
@@ -1,30 +1,17 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 
 import MyWeb from './src/WebView';
+import Permission from './src/Permission';
+import Map from './src/MapPage';
 
 function App(): React.JSX.Element {
-
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar />
       <MyWeb />
+      {/* <Permission /> */}
+      {/* <Map /> */}
     </SafeAreaView>
   );
 }
@@ -33,7 +20,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-between',
   },
 });
 
