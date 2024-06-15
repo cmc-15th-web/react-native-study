bff49e8c846887074ff2a314c14b341033f31031
joseph0926 <joseph0926@naver.com>
2024-06-16 08:27:19 +0900

    chore: [1] expo를 이용한 초기 프로젝트 구성

---
+++ Younghoon/week3/app/_layout.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+export default function _layout() {
+  return (
+    <View>
+      <Text>_layout</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
