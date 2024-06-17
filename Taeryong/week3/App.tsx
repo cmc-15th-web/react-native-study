6a70adc04279a5d3a77119425171ead88a34815f
TaegeonKim <aksen5240@gmail.com>
2024-06-18 00:53:47 +0900

    feat: 앱 진입 시 카메라 및 앨범 권한 동의 요청 구현

--- Taeryong/week3/App.tsx
+++ Taeryong/week3/App.tsx
@@ -2,6 +2,7 @@ import SplashScreen from 'react-native-splash-screen';
 import {NavigationContainer} from '@react-navigation/native';
 import GlobalNavigation from '@/components/navigation/GlobalNavigation';
 import {useEffect} from 'react';
+import PermissionCheck from '@/utils/PermissionCheck';
 
 const App = () => {
   useEffect(() => {
@@ -9,6 +10,7 @@ const App = () => {
       SplashScreen.hide();
     }, 2000); //스플래시 활성화 시간
   });
+  const accessability = PermissionCheck(); // accessability = {"album": true, "camera": true}
   return (
     <NavigationContainer>
       <GlobalNavigation />
