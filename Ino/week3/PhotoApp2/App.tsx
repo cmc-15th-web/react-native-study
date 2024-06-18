b92239dc7adafeef123037edd21cf0484715fb21
yeongeun <duddms0190@naver.com>
2024-06-19 02:02:39 +0900

    feat: Create React Native App

---
+++ Ino/week3/PhotoApp2/App.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import HomePage from './src/pages/HomePage';
+
+function App(): React.JSX.Element {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  };
+
+  return (
+    <SafeAreaView style={backgroundStyle}>
+      <StatusBar
+        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
+        backgroundColor={backgroundStyle.backgroundColor}
+      />
+      <HomePage />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
---
+++ Ino/week3/PhotoApp2/src/pages/HomePage.tsx
@@ -0,0 +1,13 @@
+import {StyleSheet, Text, View} from 'react-native';
+
+function HomePage(): React.JSX.Element {
+  return (
+    <View>
+      <Text>Home 화면</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default HomePage;
