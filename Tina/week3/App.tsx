25e9035374185c40ac42a28bb9fa903e09fa9d33
kwaksj329 <kwaksj329@gmail.com>
2024-06-20 03:10:29 +0900

    feat: 그라데이션 Text 추가

--- Tina/week3/App.tsx
+++ Tina/week3/App.tsx
@@ -1,7 +1,9 @@
 import React, {useEffect} from 'react';
-import {SafeAreaView} from 'react-native';
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
 import SplashScreen from 'react-native-splash-screen';
 import {checkAllPermissions} from './src/utils/permissions';
+import THEME_COLOR from './src/styles/theme-color';
+import GradientText from './src/GradientText';
 
 function App(): React.JSX.Element {
   useEffect(() => {
@@ -10,7 +12,25 @@ function App(): React.JSX.Element {
       checkAllPermissions();
     }, 1500);
   }, []);
-  return <SafeAreaView />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <GradientText style={styles.header} text="CMC님의 사진첩" />
+    </SafeAreaView>
+  );
 }
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: THEME_COLOR.Black,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: THEME_COLOR.Purple,
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingLeft: 18,
+  },
+});
 export default App;
