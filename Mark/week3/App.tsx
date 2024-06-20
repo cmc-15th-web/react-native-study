c234d32097d593908729818d6909cc8f0bf25622
Lee-Sunho <sunho9889@naver.com>
2024-06-21 00:57:13 +0900

    feat: 안드로이드 스플래시 구현

--- Mark/week3/App.tsx
+++ Mark/week3/App.tsx
@@ -1,13 +1,17 @@
-import React, { useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {StyleSheet, SafeAreaView} from 'react-native';
 import StackNavigator from './src/components/StackNavigator';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
-import { checkPermissions } from './src/utils/permissions';
+import {checkPermissions} from './src/utils/permissions';
+import SplashScreen from 'react-native-splash-screen';
 
 function App(): React.JSX.Element {
   useEffect(() => {
-    checkPermissions();
-  }, [])
+    setTimeout(() => {
+      SplashScreen.hide();
+      checkPermissions();
+    }, 3000);
+  }, []);
 
   return (
     <GestureHandlerRootView style={styles.container}>
--- Mark/week3/android/app/src/main/java/com/week3/MainActivity.kt
+++ Mark/week3/android/app/src/main/java/com/week3/MainActivity.kt
@@ -1,22 +1,22 @@
 package com.week3
 
+import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
+import org.devio.rn.splashscreen.SplashScreen
 
 class MainActivity : ReactActivity() {
 
-  /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
-   * rendering of the component.
-   */
-  override fun getMainComponentName(): String = "week3"
+    override fun getMainComponentName(): String = "week3"
 
-  /**
-   * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
-   * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
-   */
-  override fun createReactActivityDelegate(): ReactActivityDelegate =
-      DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    override fun createReactActivityDelegate(): ReactActivityDelegate {
+        return DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        SplashScreen.show(this)  // 추가된 부분
+        super.onCreate(savedInstanceState)
+    }
 }
